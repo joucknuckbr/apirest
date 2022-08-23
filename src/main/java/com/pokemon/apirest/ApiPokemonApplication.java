70aa7dde7b65b046a0a17477b65a42b1da353389
Nuno Proença <nuno.proenca@intelitrader.com.br>
2022-08-23 16:56:10 -0300

    first commit

---
+++ src/main/java/com/pokemon/apirest/ApiPokemonApplication.java
@@ -0,0 +1,13 @@
+package com.pokemon.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPokemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPokemonApplication.class, args);
+	}
+
+}
